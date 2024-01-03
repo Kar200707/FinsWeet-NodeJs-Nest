0ea5c9dfcaea6433bccb0bec229d043495b7100b
Kar200707 <harutyunyan2007karen@gmail.com>
2024-01-03 11:09:50 +0400

    fix hello world bug

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import * as process from "process";
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class AppService {  }
