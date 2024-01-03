484cc1869857437caccc0e66998bbcb18b67ad3e
Kar200707 <harutyunyan2007karen@gmail.com>
2024-01-03 11:13:53 +0400

    fix hello world bug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from './app.service';
+import { Controller } from "@nestjs/common";
 
-@Controller('')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  gert():string {
-    return this.appService.getHello()
-  }
-}
+@Controller()
+export class AppController { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import * as process from "process";
-
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class AppService {  }
